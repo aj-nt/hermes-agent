53d4ee32eb6f62c427974782daac8584002c8cee
AJ <q19dcp@gmail.com>
2026-04-23 00:13:35 -0400

    feat(checkpoint): add checkpoint usage guidance to system prompt

--- run_agent.py
+++ run_agent.py
@@ -86,6 +86,7 @@ from agent.error_classifier import classify_api_error, FailoverReason
 from agent.prompt_builder import (
     DEFAULT_AGENT_IDENTITY, PLATFORM_HINTS,
     MEMORY_GUIDANCE, SESSION_SEARCH_GUIDANCE, SKILLS_GUIDANCE,
+    CHECKPOINT_GUIDANCE,
     build_nous_subscription_prompt,
 )
 from agent.model_metadata import (
@@ -4478,6 +4479,8 @@ class AIAgent:
             tool_guidance.append(SESSION_SEARCH_GUIDANCE)
         if "skill_manage" in self.valid_tool_names:
             tool_guidance.append(SKILLS_GUIDANCE)
+        if "checkpoint" in self.valid_tool_names:
+            tool_guidance.append(CHECKPOINT_GUIDANCE)
         if tool_guidance:
             prompt_parts.append(" ".join(tool_guidance))
 
